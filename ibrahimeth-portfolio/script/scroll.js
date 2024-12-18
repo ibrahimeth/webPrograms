bdf0dd686df7b46ac150fc8ffa367756f9a26b73
ibrahimeth <21060671@stu.omu.edu.tr>
2024-12-18 21:27:03 +0300

    education started

--- ibrahimeth-portfolio/script/scroll.js
+++ ibrahimeth-portfolio/script/scroll.js
@@ -39,3 +39,21 @@ function checkBoxes() {
 }
 window.addEventListener('scroll', checkBoxes);
 window.addEventListener('load', checkBoxes);
+
+
+const scrollTopBtn = document.getElementById('scrollTopBtn');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) { 
+    scrollTopBtn.classList.add('show');
+  } else {
+    scrollTopBtn.classList.remove('show');
+  }
+});
+
+scrollTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0, 
+    behavior: 'smooth' 
+  });
+});
